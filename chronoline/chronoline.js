d2fd718c2f632f5a0184089308022acf80636a90
Padhraic <pcoogan@gmail.com>
2018-05-21 23:43:29 +0900

    bump version

--- chronoline/chronoline.js
+++ chronoline/chronoline.js
@@ -1,4 +1,4 @@
-// chronoline.js v0.1.5
+// chronoline.js v0.1.6
 // by Kevin Leung for Zanbato, https://zanbato.com
 // MIT license at https://github.com/StoicLoofah/chronoline.js/blob/master/LICENSE.md
 
@@ -131,7 +131,7 @@ function forwardWeek(date){
 
 function Chronoline(domElement, events, options) {
     var t = this;
-    t.VERSION = "0.1.5";
+    t.VERSION = "0.1.6";
 
     //sanitize Chronoline parameters
     //initilize events to emplty arry if null
